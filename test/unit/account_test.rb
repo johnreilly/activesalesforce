e07dddfc69cab5b43c227f9959a9dea7d9319a3f
dchasman <dchasman@d7a8b676-ae08-0410-af3b-bd654c4a315c>
2006-01-18 20:45:13 +0000

    Updated example Rails utest
    git-svn-id: svn://rubyforge.org/var/svn/activesfdc/trunk@36 d7a8b676-ae08-0410-af3b-bd654c4a315c

--- test/unit/account_test.rb
+++ test/unit/account_test.rb
@@ -1,42 +1,44 @@
 require File.join(File.dirname(__FILE__), '../../config/boot')
-require  '~/dev/activesfdc/trunk/ActiveSalesforce/src/salesforce_connection_adapter'
+
+require_gem 'activesalesforce'
+require  'salesforce_connection_adapter'
+
 require File.dirname(__FILE__) + '/../test_helper'
 
 
 
 class AccountTest < Test::Unit::TestCase
-
+  def setup
+    ActiveRecord::Base.allow_concurrency = true
+  end
+  
   def test_get_account 
-    products = Account.find(:all)
-    #pp products
-      
-    products.each { |product| puts "#{product.Name}, #{product.Id}, #{product.LastModifiedById}, #{product.Description}" }
+    accounts = Account.find(:all)
+
+    #accounts.each { |account| puts "#{account.Name}, #{account.Id}, #{account.LastModifiedById}" }
   
     acme = Account.find(:first, :conditions => ["Name = 'Acme'"])
-    puts acme.Name
   
     acme = Account.find_by_Id(acme.Id)
-    puts acme.Name
 
     acme = Account.find_by_Name_and_LastModifiedById('salesforce.com', acme.LastModifiedById)
-    puts acme.Name
   end
 
+   
   def test_update_account
-    #return
-    
     acme = Account.find_by_Name('Acme')
-    puts acme.Name
         
-    acme.Website = "http://www.dutchforce.com/myImage2.jpg"
+    acme.Website = "http://www.dutchforce.com/#{Time.now}.jpg"
+    acme.LastModifiedDate = Time.now
     
     acme.save
   end
-    
-  def test_xcreate_account
+ 
+  def test_create_account
     dutchCo = Account.new 
     dutchCo.Name = "DutchCo"
+    dutchCo.Website = "www.dutchco.com"
     dutchCo.save
-  end
-  
+  end 
+       
 end
