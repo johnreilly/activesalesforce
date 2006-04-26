4466b2855d9f15adadc0d82f745f714117bca139
dchasman <dchasman@d7a8b676-ae08-0410-af3b-bd654c4a315c>
2006-04-26 21:51:30 +0000

    Fix for incorrect use of chop(3)
    git-svn-id: svn://rubyforge.org/var/svn/activesfdc/trunk@260 d7a8b676-ae08-0410-af3b-bd654c4a315c

--- lib/asf_adapter.rb
+++ lib/asf_adapter.rb
@@ -585,7 +585,7 @@ module ActiveRecord
             end
             
             # Handle references to custom objects
-            reference_to = reference_to.chop(3).capitalize if reference_to.match(/__c$/)
+            reference_to = reference_to.chomp("__c").capitalize if reference_to.match(/__c$/)
             
             begin
               referenced_klass = class_from_entity_name(reference_to)
